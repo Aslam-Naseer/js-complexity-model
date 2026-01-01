0e9b6421c333b05cf466c60df6e0745fea40122d
Aslam-Naseer <aslamnaseer22@gmail.com>
2026-01-01 16:20:17 +0000

    Select a subset from full set

--- notebooks/utils/preprocess_code.py
+++ notebooks/utils/preprocess_code.py
@@ -2,7 +2,7 @@ import re
 
 
 def preprocess_js_code(datapoint, max_len=2000):
-    code = datapoint.get("original_string", "")
+    code = datapoint.get("code", "")
 
     code = code.strip()
     code = re.sub(r"\s+", " ", code)
@@ -13,7 +13,16 @@ def preprocess_js_code(datapoint, max_len=2000):
 
 
 def convert_to_arrow(js_code):
-    pattern = r"function\s+([a-zA-Z0-9_$]+)\s*\((.*?)\)\s*\{"
-    replacement = r"const \1 = (\2) => {"
+    pattern = r"(async\s+)?function\s*([a-zA-Z0-9_$]+)?\s*\((.*?)\)\s*\{"
 
-    return re.sub(pattern, replacement, js_code)
+    def replace_func(match):
+        is_async = match.group(1) if match.group(1) else ""
+        name = match.group(2)
+        params = match.group(3)
+
+        if name:
+            return f"const {name} = {is_async}({params}) => {{"
+        else:
+            return f"{is_async}({params}) => {{"
+
+    return re.sub(pattern, replace_func, js_code)
